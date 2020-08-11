0c340f9dd362f32cdfacb2e90fd9c8af2c583a80
lexus2727 <lexus2424@yahoo.com>
2020-08-11 19:47:31 -0400

    addJS

---
+++ public/mytemp.js
@@ -0,0 +1,5 @@
+function CelsiusConverter(){
+    document.converter.fahrenheit.value = (document.converter.celsius.value * 9 / 5) + 32
+    document.converter.kelvin.value = document.converter.celsius.value * 1 + 273.15
+    }
+   
