2713a18308bae8e96e87fa12579358dbb6359a11
lexus2727 <lexus2424@yahoo.com>
2020-08-11 22:47:09 -0400

    Updates

---
+++ precache-manifest.b63ee69eb399633aa152bc5ace7f6163.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbbbc50ec0394f6f27011d0d9ae30f0",
+    "url": "/temp_converter/index.html"
+  },
+  {
+    "revision": "b3f8984e8d71c0b62957",
+    "url": "/temp_converter/static/js/main.7b911ba6.chunk.js"
+  },
+  {
+    "revision": "c981b1b818d23e2a046f",
+    "url": "/temp_converter/static/js/runtime-main.1d3f9a77.js"
+  }
+]);
---
+++ static/js/main.7b911ba6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],[function(p,n,s){p.exports=s(1)},function(p,n){}],[[0,1]]]);
+//# sourceMappingURL=main.7b911ba6.chunk.js.map
